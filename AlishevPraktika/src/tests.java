bb8f9f6ad4f2b6b17f33de8352bd61e48e76f141
Kuzin Dmitrii <exelent26@gmail.com>
2024-07-18 23:11:22 +0300

    to main

--- AlishevPraktika/src/tests.java
+++ AlishevPraktika/src/tests.java
@@ -2,7 +2,7 @@ public class tests {
 
     public static void main(String[] args) {
         int n = "Строка".length();
-        System.out.println("32155433453545fddfwfwdf43OMFGFROMWINDAandmac");
+        System.out.println("32155433453545fddfwfwdf43OMFGFROMWINDAandmacredakt");
         String[] array = {"Tic", "Tac", "Toe"};
         int  w = array.length;
         System.out.println(w);
