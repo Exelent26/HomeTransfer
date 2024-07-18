dfe86b1af035414020f79f930604c99c3f5a39ab
Dmitrii Kuzin <exelent26@gmail.com>
2024-07-18 22:02:32 +0300

    trying to synchrone

--- AlishevPraktika/src/tests.java
+++ AlishevPraktika/src/tests.java
@@ -2,7 +2,7 @@ public class tests {
 
     public static void main(String[] args) {
         int n = "Строка".length();
-        System.out.println("321");
+        System.out.println("3215543345354543");
         String[] array = {"Tic", "Tac", "Toe"};
         int  w = array.length;
         System.out.println(w);
