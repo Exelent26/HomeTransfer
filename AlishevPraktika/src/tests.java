c1f6f1b4ffbce5f5eb579c69f12b530ec995c2de
Kuzin Dmitrii <exelent26@gmail.com>
2024-07-18 21:42:19 +0300

    add 123 to tests

--- AlishevPraktika/src/tests.java
+++ AlishevPraktika/src/tests.java
@@ -2,7 +2,7 @@ public class tests {
 
     public static void main(String[] args) {
         int n = "Строка".length();
-        System.out.println(n);
+        System.out.println("123");
         String[] array = {"Tic", "Tac", "Toe"};
         int  w = array.length;
         System.out.println(w);
